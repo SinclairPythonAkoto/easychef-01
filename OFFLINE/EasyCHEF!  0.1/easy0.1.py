5d2e9d15fcfaff6ba1e29ac4f9bc5fd525115e9c
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-22 22:37:15 +0100

    easychef OFFLINE search created - still needs to fix bugs where list index is out of range..

--- OFFLINE/EasyCHEF!  0.1/easy0.1.py
+++ OFFLINE/EasyCHEF!  0.1/easy0.1.py
@@ -4,58 +4,18 @@ import requests
 app = Flask(__name__)
 
 @app.route('/')
+def start_pg():
+	return render_template('start.html')
+
+@app.route('/home')
 def home():
 	return render_template('home.html')
 
-@app.route('/random', methods=["GET", "POST"])
-def random():
-	if request.method == 'GET':
-		url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random"
+# @app.route('/random', methods=["GET", "POST"])
+# def random():
+# 	pass
 
-		querystring = {"number":"1",}
-
-		headers = {
-		    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-		    'x-rapidapi-key': "670505dc3fmsh9cd923658f74705p10c323jsnc8539188f679"
-		    }
-
-		response = requests.request("GET", url, headers=headers, params=querystring)
-
-		# print(response.text)
-
-		# Dictionary key='recipe' value=list(dict())
-		data = response.json()
-
-		# List with dictionary inside
-		data = data['recipes']
-
-		# Accessing the dictionary
-		recipe = data[0]
-
-		# Name of recipe
-		title = data[0]['title']
-
-
-		# To get Acces to Ingredients
-		extendedIng = data[0]['extendedIngredients']
-		ex = extendedIng[0]
-		rec = recipe['extendedIngredients']
-
-		# # List of Ingredients
-		# for x in rec:
-		# 	print(x['originalString'])
-
-		# Recipe instructions
-		instruct = data[0]['instructions']
-
-		# Source Url of recipe
-		source = data[0]['sourceUrl']
-
-		# Acces to just one of the ingredients (we need all)
-		x1 = ex['originalString']
-
-		return render_template('random_recipe.html', title=title, rec=rec, instruct=instruct, source=source)
-
+		
 @app.route('/search', methods=['GET', 'POST'])
 def search():
 	if request.method == 'GET':
@@ -63,39 +23,39 @@ def search():
 	else:
 		ing = request.form.get("ingredients")
 
-		if ing:
+		url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex"
 
-			url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex"
-			querystring = {"query":f"{ing}","includeIngredients":"onions, garlic, tomato","excludeIngredients":"bacon","ranking":"2","instructionsRequired":"true","addRecipeInformation":"true","limitLicense":"true","number":"1"}
+		querystring = {"query":f"{ing}","includeIngredients":"onions, garlic, tomato","excludeIngredients":"bacon","ranking":"2","instructionsRequired":"true","addRecipeInformation":"true","limitLicense":"true","number":"1"}
 
-			headers = {
-			    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-			    'x-rapidapi-key': "670505dc3fmsh9cd923658f74705p10c323jsnc8539188f679"
-			    }
-			response = requests.request("GET", url, headers=headers, params=querystring)
-			data = response.json()
+		headers = {
+		    'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		    'x-rapidapi-key': "670505dc3fmsh9cd923658f74705p10c323jsnc8539188f679"
+		    }
 
-			# Accessing dictionary
-			data = data['results'][0]
+		response = requests.request("GET", url, headers=headers, params=querystring)
+		data = response.json()
 
-			# Title of recipe
-			title = data['title']
+		# Accessing dictionary
+		data = data['results'][0]
 
-			# Time to cook
-			duration = data['readyInMinutes']
+		# Title of recipe
+		title = data['title']
 
-			# image URL
-			image = data['image']
+		# Time to cook
+		duration = data['readyInMinutes']
 
-			# URL source
-			url_source = data['sourceUrl']
+		# URL source
+		url_source = data['sourceUrl']
 
-			# List of recipes
-			analyzedInstructions = data['analyzedInstructions'][0]
-			steps = analyzedInstructions['steps']
+		# image URL
+		image = data['image']
+
+		# List of recipes
+		analyzedInstructions = data['analyzedInstructions'][0]
+		steps = analyzedInstructions['steps']
 
 
-			return render_template('search.html', title=title, duration=duration, image=image, url_source=url_source, steps=steps)
+		return render_template('search.html', title=title, duration=duration, image=image, url_source=url_source, steps=steps)
 
 if __name__ == '__main__':
     app.run(debug=True)
