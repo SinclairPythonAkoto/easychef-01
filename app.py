3c71665f64786ce25d97048c81b6181544d12f12
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-31 08:55:20 +0100

    file update news/food feed

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def easysearch_feedback():
 @app.route('/foods_feed')
 def foods_feed(): 
 	url = ('https://newsapi.org/v2/everything?'
-       'q=health&nutrition&cooking&food&culinary&chef&recipes&kitchen&cuisine&'
+       'q=cooking&food&culinary&chef&recipes&kitchen&cuisine&health&nutrition&'
        'from=2019-08-31&'
        'sortBy=popularity&'
        'apiKey=134b50d54e2948399817da7fa47477b0')
